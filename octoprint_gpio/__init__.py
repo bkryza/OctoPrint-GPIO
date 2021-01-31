1a1a3e94f8ce44ec3a078202060b95306e0f835a
Bartek Kryza <bkryza@gmail.com>
2021-01-31 23:00:55 +0100

    Added initial tab interface

--- octoprint_gpio/__init__.py
+++ octoprint_gpio/__init__.py
@@ -12,20 +12,37 @@ from __future__ import absolute_import
 import octoprint.plugin
 
 from enum import Enum
-
-# {"gpio": 0, "type": ["UNDEFINED"]},
-# {"gpio": 1, "type": ["UNDEFINED"]},
-# {"gpio": 2, "type": ["INPUT"], "label": "Enclosure lock sensor", "pullUp": True, "activeState": True, "bounceTime": 0},
-# {"gpio": 3, "type": ["OUTPUT"], "label": "Enclosure light", "initialValue": False},
-# {"gpio": 4, "type": ["PWM"], "label": "RGB backlight"},
-# {"gpio": 6, "type": ["SERVO"], "label": ""},
-
+from gpiozero.pins.mock import MockFactory
+from gpiozero import DigitalOutputDevice, Button, PWMOutputDevice, Servo
 
 class GpioPlugin(
+        octoprint.plugin.StartupPlugin,
         octoprint.plugin.SettingsPlugin,
         octoprint.plugin.AssetPlugin,
         octoprint.plugin.TemplatePlugin,
+        octoprint.plugin.SimpleApiPlugin,
 ):
+    def __init__(self):
+        self.pins = {}
+
+    ##~~ StartupPlugin mixin
+    def on_after_startup(self):
+        self.update_pin_bindings()
+
+    ##~~ SimpleApiPlugin mixin
+    def get_api_commands(self):
+        return dict(set_gpio=["gpio", "value"])
+
+    def on_api_command(self, command, data):
+        import flask
+        if command == "set_gpio":
+            self._logger.error("set_gpio called with {data}".format(data=data))
+            pin = int(data["gpio"])
+            value = int(data["value"])
+            if pin not in self.pins:
+                return flask.make_response(
+                    "Pin {pin} not defined".format(pin=pin), 404)
+            self.set_gpio(pin, value)
 
     ##~~ SettingsPlugin mixin
     def get_settings_defaults(self):
@@ -36,6 +53,12 @@ class GpioPlugin(
             } for k in range(0, 27)],
             gpioTypes=["UNDEFINED", "OUTPUT", "INPUT", "PWM", "SERVO"],
             gpioFactories=["Native", "PiGPIO", "RPIO", "RPi.GPIO", "Mock"])
+
+    def on_settings_initialized(self):
+        pass  # self.update_pin_bindings()
+
+    def get_settings_version(self):
+        return 1
 
     ##~~ AssetPlugin mixin
     def get_assets(self):
@@ -62,6 +85,54 @@ class GpioPlugin(
             pip=
             "https://github.com/bkryza/OctoPrint-GPIO/archive/{target_version}.zip",
         ))
+
+    def update_pin_bindings(self):
+        bindings = self._settings.get(["bindings"])
+        self._logger.error(str(bindings))
+        for gpio in bindings:
+            pin = int(gpio["gpio"])
+            if gpio["type"][0] == "OUTPUT":
+                self._logger.error(
+                    "Binding DigitalOutputDevice to pin {pin}".format(pin=pin))
+                self.pins[pin] = DigitalOutputDevice(pin,
+                                     active_high=True,
+                                     initial_value=True,
+                                     pin_factory=MockFactory())
+            if gpio["type"][0] == "INPUT":
+                self._logger.error(
+                    "Binding Button to pin {pin}".format(pin=pin))
+                self.pins[pin] = Button(pin,
+                                        active_state=None,
+                                        pull_up=True,
+                                        bounce_time=0.01,
+                                        pin_factory=MockFactory())
+            if gpio["type"][0] == "PWM":
+                self._logger.error("Binding PWM to pin {pin}".format(pin=pin))
+                self.pins[pin] = PWMOutputDevice(pin,
+                                     active_high=True,
+                                     initial_value=0.0,
+                                     frequency=100,
+                                     pin_factory=MockFactory())
+            if gpio["type"][0] == "SERVO":
+                self._logger.error(
+                    "Binding Servo to pin {pin}".format(pin=pin))
+                self.pins[pin] = Servo(pin,
+                                       initial_value=0.0,
+                                       min_pulse_width=1 / 1000,
+                                       max_pulse_width=2 / 1000,
+                                       frame_width=20 / 1000,
+                                       pin_factory=MockFactory())
+
+    def set_gpio(self, pin, value):
+        self._logger.error("Setting pin {pin} to value {value}".format(
+            pin=pin, value=value))
+        device = self.pins[pin]
+        if isinstance(device, DigitalOutputDevice):
+            self.pins[pin].value = bool(value)
+        if isinstance(device, PWMOutputDevice):
+            self.pins[pin].value = value/100.0
+        if isinstance(device, Servo):
+            self.pins[pin].value = value/100.0
 
 
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
--- octoprint_gpio/static/js/gpio.js
+++ octoprint_gpio/static/js/gpio.js
@@ -17,6 +17,7 @@ $(function() {
     self.outputControls = ko.observableArray(["Indicator"]);
     self.pwmControls = ko.observableArray(["Input", "Slider"]);
     self.servoControls = ko.observableArray(["Input", "Slider"]);
+    self.enabledControls = ko.observableArray();
 
     self.loginStateViewModel = parameters[0];
     self.settingsViewModel = parameters[1];
@@ -25,6 +26,24 @@ $(function() {
       self.gpioTypes(self.settingsViewModel.settings.plugins.gpio.gpioTypes());
       self.gpioFactories(self.settingsViewModel.settings.plugins.gpio.gpioFactories())
       self.gpioSettings(self.settingsViewModel.settings.plugins.gpio.bindings());
+      var controls = [];
+      ko.utils.arrayForEach(self.gpioSettings(), function(item) {
+        value = ko.observable();
+        value.subscribe(function(newValue) {
+          OctoPrint.simpleApiCommand("gpio", "set_gpio", {
+              "gpio": parseInt(item.gpio()),
+              "value": parseInt(newValue)
+            })
+            .done(function(data) {});
+        });
+
+        if (item.type()[0] !== 'UNDEFINED') {
+          item.value = value;
+          item.id = "GPIO" + item.gpio();
+          controls.push(item);
+        }
+      });
+      self.enabledControls(controls);
     };
 
     self.onBeforeBinding = function() {
@@ -35,6 +54,70 @@ $(function() {
       self.bindFromSettings();
     };
   }
+
+  // Custom binding for slider control
+  ko.bindingHandlers.sliderRange = {
+    init: function(element, valueAccessor, allBindingsAccessor) {
+      var options = allBindingsAccessor().sliderOptions || {};
+      $(element).slider(options);
+      ko.utils.registerEventHandler(element, "slidechange", function(event, ui) {
+        var observable = valueAccessor();
+        observable.Min(ui.values[0]);
+        observable.Max(ui.values[1]);
+      });
+      ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+        $(element).slider("destroy");
+      });
+      ko.utils.registerEventHandler(element, "slide", function(event, ui) {
+        var observable = valueAccessor();
+        observable.Min(ui.values[0]);
+        observable.Max(ui.values[1]);
+      });
+    },
+    update: function(element, valueAccessor, allBindingsAccessor) {
+      var value = ko.utils.unwrapObservable(valueAccessor());
+      if (isNaN(value.Min())) value.Min(0);
+      if (isNaN(value.Max())) value.Max(0);
+
+      $(element).slider("option", allBindingsAccessor().sliderOptions);
+      $(element).slider("values", 0, value.Min());
+      $(element).slider("values", 1, value.Max());
+    }
+  };
+
+  // Custom binding for Bootstrap radio button group
+  ko.bindingHandlers.radioButtonGroup = {
+    init: function(element, valueAccessor, allBindings, viewModel, context) {
+      var $buttons, $element, observable;
+      observable = valueAccessor();
+      if (!ko.isWriteableObservable(observable)) {
+        throw "You must pass an observable or writeable computed";
+      }
+      $element = $(element);
+      if ($element.hasClass("btn")) {
+        $buttons = $element;
+      } else {
+        $buttons = $(".btn", $element);
+      }
+      elementBindings = allBindings();
+      $buttons.each(function() {
+        var $btn, btn, radioValue;
+        btn = this;
+        $btn = $(btn);
+        radioValue = elementBindings.radioValue ||
+          $btn.attr("data-value") || $btn.attr("value") || $btn.text();
+        $btn.on("click", function() {
+          observable(ko.utils.unwrapObservable(radioValue));
+        });
+        return ko.computed({
+          disposeWhenNodeIsRemoved: btn,
+          read: function() {
+            $btn.toggleClass("active", observable() === ko.utils.unwrapObservable(radioValue));
+          }
+        });
+      });
+    }
+  };
 
   /* view model class, parameters for constructor, container to bind to
    * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
